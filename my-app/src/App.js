f9122453ba882de79aa55d7ba4b0ca6f82f35d44
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-02-18 20:00:26 +0400

    First step

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./Logo-profile" className="Logo-profile" alt="logo"/>
+        <p>
+          GrustnyyVeselchak
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/GrustnyyVeselchak"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
